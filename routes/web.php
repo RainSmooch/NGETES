fad5cf8efb80ed224edb64e49066eba7c863d53f
RainSmooch <olishmuhammad@gmail.com>
2025-04-24 08:05:55 +0700

    Mukhlish

---
+++ routes/web.php
@@ -0,0 +1,13 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('guru.datasiswa');
+});
+Route::get('/1', function () {
+    return view('guru.dashboard');
+});
+Route::get('/2', function () {
+    return view('guru.laporan');
+});
